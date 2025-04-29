6f2eacadf7fb20be90bbcd30189a3fe8b95f0af7
João Antônio Corrêa <joaoantoniocorreaf@gmail.com>
2025-04-28 23:08:18 -0300

    structuring the code and adding logs

--- scripts/data_collector.py
+++ scripts/data_collector.py
@@ -1,15 +1,38 @@
 import pandas as pd
 import requests
+import logging
 from datetime import datetime
+import os
+
+logging.basicConfig(
+    filename=os.path.join('logs', 'collector.log'),
+    level=logging.INFO,
+    format='%(asctime)s:%(levelname)s:%(message)s'
+)
 
 def fetch_prices():
-    response = requests.get("https://api.binance.com/api/v3/ticker/price")
-    data = response.json()
-    df = pd.DataFrame(data)
-    df["timestamp"] = datetime.utcnow()
-    return df
     
+    try:
+        response = requests.get("https://api.binance.com/api/v3/ticker/price")
+        data = response.json()
+        df = pd.DataFrame(data)
+        df["timestamp"] = datetime.utcnow()
+        logging.info(f"{len(df)} symbols collected from Binance")
+        return df
+    
+    except Exception as e:
+          logging.error(f"data extraction error {e}")  
+          raise
+          
+def save_to_csv(path, df):
+    try:
+        df.to_csv(path, index=False)
+        logging.info(f"Data saved successfully in {path}")
+    except Exception as e:
+        logging.error(f"Error when saving data {e}")
+        raise
     
 if __name__ == '__main__':
+    os.makedirs('data/raw', exist_ok=False)
     df = fetch_prices()
-    df.to_csv("raw_crypto_prices.csv", index=False)
+    save_to_csv("data/raw/raw_crypto_prices.csv", df)
--- scripts/spark_etl.py
+++ scripts/spark_etl.py
@@ -1,19 +1,43 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, avg, lag
 from pyspark.sql.window import Window
+import logging
+import os
 
-spark = SparkSession.builder.appName("CryptoETL").getOrCreate()
+logging.basicConfig(
+    filename=os.path.join('logs', 'etl.log'),
+    level=logging.INFO,
+    format='%(asctime)s:%(levelname)s:%(message)s'
+)
 
-df = spark.read.csv("raw_crypto_prices.csv", header=True, inferSchema=True)
+def start_spark():
+    return SparkSession.builder.appName("CryptoETL").getOrCreate()
+    
+def process_data(spark, input_path, output_path):
+    
+    try:
+    
+        df = spark.read.csv(input_path, header=True, inferSchema=True)
 
-df = df.withColumn("price", col("price").cast("float"))
+        df = df.withColumn("price", col("price").cast("float"))
 
-windowSpec = Window.partitionBy("symbol").orderBy("timestamp")
+        windowSpec = Window.partitionBy("symbol").orderBy("timestamp")
 
-df = df.withColumn("moving_avg", avg("price").over(windowSpec.rowsBetween(-4, 0)))
+        df = df.withColumn("moving_avg", avg("price").over(windowSpec.rowsBetween(-4, 0)))
 
-df = df.withColumn("previous_price", lag("price").over(windowSpec))
+        df = df.withColumn("previous_price", lag("price").over(windowSpec))
 
-df = df.withColumn("price_change_pct", ((col("price") - col("previous_price")) / col("previous_price")) * 100)
+        df = df.withColumn("price_change_pct", ((col("price") - col("previous_price")) / col("previous_price")) * 100)
 
-df.write.mode("overwrite").parquet("processed_crypto_data.parquet")
+        df.write.mode("overwrite").parquet(output_path)
+        
+        logging.info(f"Data saved in {output_path}")
+
+    except Exception as e:
+        logging.error(f"Error when processed data {e}")
+        raise
+    
+if __name__ == '__main__':
+    os.makedirs("data/processed", exist_ok=True)
+    spark = start_spark()
+    process_data(spark, "data/raw/raw_crypto_prices.csv", "data/processed/processed_crypto_data.parquet")
